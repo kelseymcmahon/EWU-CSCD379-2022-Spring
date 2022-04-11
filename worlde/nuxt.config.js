e0fc4289f6433ce0cab97aedd88c3f64b49bf06a
Kelsey McMahon <kelsey.mcmahon@hotmail.com>
2022-04-10 21:04:15 -0700

    Adding visual edits

--- worlde/nuxt.config.js
+++ worlde/nuxt.config.js
@@ -47,12 +47,12 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      light: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          primary: colors.indigo.darken2,
           accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+          secondary: colors.orange.darken2,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
