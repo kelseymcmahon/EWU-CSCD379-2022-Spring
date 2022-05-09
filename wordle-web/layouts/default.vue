132cce8e777b7a761f7750d27af68a7c989b682b
Kelsey McMahon <40438334+kelseymcmahon@users.noreply.github.com>
2022-05-09 10:36:38 -0700

    adding scorestat page

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -34,6 +34,13 @@
             <v-list-item-title>Game</v-list-item-title>
           </v-list-item>
 
+          <v-list-item nuxt to="/ScoreStats">
+            <v-list-item-icon>
+              <v-icon>mdi-equalizer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Score Stats</v-list-item-title>
+          </v-list-item>
+
           <v-list-item nuxt to="/About">
             <v-list-item-icon>
               <v-icon>mdi-cat</v-icon>
