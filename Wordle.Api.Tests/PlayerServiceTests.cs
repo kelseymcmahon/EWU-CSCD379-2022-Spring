a8cbf39276507d81968a465682b740a69de82e52
Kelsey McMahon <40438334+kelseymcmahon@users.noreply.github.com>
2022-05-11 15:48:30 -0700

    fixing tests that got deleted

--- Wordle.Api.Tests/PlayerServiceTests.cs
+++ Wordle.Api.Tests/PlayerServiceTests.cs
@@ -14,31 +14,71 @@ namespace Wordle.api.Tests;
 public class PlayerServiceTests
 {
     private AppDbContext _context;
+    private PlayerService service;
 
-    public PlayerServiceTests()
+    [TestInitialize]
+    public void testInit()
     {
         var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
             .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Wordle.Api.Tests;Trusted_Connection=True;MultipleActiveResultSets=true");
         _context = new AppDbContext(contextOptions.Options);
         _context.Database.Migrate();
-        PlayerService.Seed(_context);
+
+        service = new PlayerService(_context);
+
+        _context.Players.Add(new Player()
+        {
+            Name = "Paul",
+            GameCount = 10,
+            AverageAttempts = 2.3,
+            AverageSeconds = 20
+        });
+
+        _context.Players.Add(new Player()
+        {
+            Name = "Paulina",
+            GameCount = 5,
+            AverageAttempts = 3.0,
+            AverageSeconds = 100
+        });
+
+        _context.Players.Add(new Player()
+        {
+            Name = "Donkey",
+            GameCount = 3,
+            AverageAttempts = 4.0,
+            AverageSeconds = 50
+        });
+
+        _context.SaveChanges();
+    }
+
+    [TestCleanup]
+    public void testClean()
+    {
+        _context.Players.Remove(service.GetPlayers().First(x => x.Name == "Paul"));
+        _context.Players.Remove(service.GetPlayers().First(x => x.Name == "Paulina"));
+        _context.Players.Remove(service.GetPlayers().First(x => x.Name == "Donkey"));
+
+        _context.SaveChanges();
     }
 
     [TestMethod]
     public void GetPlayers()
     {
-        PlayerService service = new PlayerService(_context);
-        Assert.AreEqual(3, service.GetPlayers().Count());
+        Assert.AreEqual(6, service.GetPlayers().Count());
     }
 
     [TestMethod]
     public void Update_AddsNewPlayer()
     {
-        //PlayerService service = new PlayerService(_context);
-        //service.Update("Kelsey", 1, 5, 20);
-        //Assert.AreEqual("Kelsey", service.GetPlayers().First(x => x.Name == "Kelsey").Name);
-        //Assert.AreEqual(11, service.GetPlayers().First(x => x.Name == "Kelsey").GameCount);
-        //Assert.AreEqual(2, service.GetPlayers().First(x => x.Name == "Kelsey").AverageAttempts);
+        Player player = service.GetPlayers().First(x => x.Name == "Paul").Clone();
+
+        //double newAverage = Math.Round(player.AverageAttempts + 5 / player.GameCount++, 2);
+        service.Update("Paul", 1, 5, 30);
+
+        Assert.AreEqual(player.GameCount + 1, service.GetPlayers().First(x => x.Name == "Paul").GameCount);
+        Assert.AreEqual(2.55, service.GetPlayers().First(x => x.Name == "Paul").AverageAttempts);
     }
 
     [TestMethod]
