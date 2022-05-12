0c3f598b7cbea4e6a5bb082c1c36396bfb5d55da
Kelsey McMahon <40438334+kelseymcmahon@users.noreply.github.com>
2022-05-11 20:34:34 -0700

    changing title

--- wordle-web/pages/PlayerScores.vue
+++ wordle-web/pages/PlayerScores.vue
@@ -2,7 +2,7 @@
   <v-container fluid fill-height justify-center>
     <v-card>
       <v-card-title>
-        <h1 class="display-1">Player Stats</h1>
+        <h1 class="display-1">Player Scores</h1>
       </v-card-title>
       <v-card-text>
         <v-simple-table>
