12e876567287de8d9d4c741b00b636a7e94d1ce5
Kelsey McMahon <kelsey.mcmahon@hotmail.com>
2022-06-07 22:10:01 -0700

    adding word service and controller

---
+++ Wordle.Api/Controllers/WordController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wordle.Api.Data;
+using Wordle.Api.Services;
+
+namespace Wordle.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WordController : ControllerBase
+{
+    private readonly WordService _service;
+
+    public WordController(WordService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("GetWordsPerPage")]
+    public IEnumerable<Word> Get(int wordsPerPage, int pageNum, string wordFilter)
+    {
+        return _service.GetWordList(wordsPerPage, pageNum, wordFilter);
+    }
+
+    [HttpGet("GetTotalWordCount")] 
+    public int Get(string wordFilter)
+    {
+        return _service.GetTotalWordCount(wordFilter);
+    }
+}
+
---
+++ Wordle.Api/Services/WordService.cs
@@ -0,0 +1,33 @@
+﻿using Wordle.Api.Data;
+using System.Linq;
+using static Wordle.Api.Data.Game;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Concurrent;
+
+namespace Wordle.Api.Services;
+
+public class WordService
+{
+    private readonly AppDbContext _context;
+
+    public WordService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Word> GetWordList(int wordsPerPage, int pageNum, string wordFilter)
+    {
+        var result = _context.Words
+            .Where(x => x.Value.StartsWith(wordFilter))
+            .Skip(pageNum * wordsPerPage - wordsPerPage)
+            .Take(wordsPerPage)
+            .OrderBy(x => x.Value);
+        return result;
+    }
+
+    public int GetTotalWordCount(string wordFilter)
+    {
+        var result = _context.Words.Where(x => x.Value.StartsWith(wordFilter)).Count();
+        return result;
+    }
+}
