dc9b3267f3309f4fa159f295d16a3365e958b045
A-Tanner <atanner5@ewu.edu>
2022-06-08 22:11:52 -0700

    Add Active field to word (to support soft delete)

---
+++ Wordle.Api/Migrations/20220609051002_ActiveField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wordle.Api.Migrations
+{
+    public partial class ActiveField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Words",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Words");
+        }
+    }
+}
